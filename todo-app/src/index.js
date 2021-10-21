02c791b6313dd2c138171bbae0a9bc9d41a56b71
volkovVA <volkov.slava@hotmail.com>
2021-10-21 21:47:34 +0300

    feat: add basic components

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -1,1 +1,50 @@
-console.log('Hello World!!!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const TodoList = () => {
+
+  const items = ['Drink Coffee', 'Build Awesome App'];
+
+  return(
+    <ul>
+      <li>{ items[0] }</li>
+      <li>{ items[1] }</li>
+    </ul>
+  );
+}
+
+const AppHeader = () => <h1>My Todo List</h1>;
+
+const SearchPanel = () => {
+
+  const searchText = 'Type here to search';
+  const searchStyle = {
+    fontSize: '25px'
+  };
+
+  return <input
+    style = { searchStyle }
+    type = "text"
+    placeholder = { searchText } />;
+}
+
+const App = () => {
+
+  const isLoggedIn = true;
+  const loginBox = <span>Log in please</span>;
+  const welcomeBox = <span>Welcome Back</span>;
+
+  const value = '<script>alert("")</script>';
+
+  return (
+    <div>
+      { value }
+      { isLoggedIn ? welcomeBox : loginBox }
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
