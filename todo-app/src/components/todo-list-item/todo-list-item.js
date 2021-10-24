b60fba14459ec9e59b2b5b9db3d356093ffebc3f
volkovVA <volkov.slava@hotmail.com>
2021-10-24 15:43:26 +0300

    fead: add handle events

--- todo-app/src/components/todo-list-item/todo-list-item.js
+++ todo-app/src/components/todo-list-item/todo-list-item.js
@@ -3,6 +3,10 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
+  onLabelClick = () => {
+    console.log(`Done: ${ this.props.label }`);
+  };
+
   render() {
 
     const { label, important = false } = this.props;
@@ -15,6 +19,7 @@ export default class TodoListItem extends Component {
     return (
       <span className="todo-list-item">
         <span
+          onClick={ this.onLabelClick }
           className="todo-list-item-label"
           style={style}>
           {label}
