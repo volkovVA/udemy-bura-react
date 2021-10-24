6f8e1d9279baa3a8cb9037531fc8f348038cd4f7
volkovVA <volkov.slava@hotmail.com>
2021-10-24 17:08:37 +0300

    fead: add delete element

--- todo-app/src/components/todo-list-item/todo-list-item.js
+++ todo-app/src/components/todo-list-item/todo-list-item.js
@@ -26,7 +26,7 @@ export default class TodoListItem extends Component {
 
   render() {
 
-    const { label } = this.props;
+    const { label, onDeleted } = this.props;
     const { done, important } = this.state;
 
     let classNames = 'todo-list-item';
@@ -53,6 +53,7 @@ export default class TodoListItem extends Component {
         </button>
 
         <button
+          onClick={ onDeleted }
           type="button"
           className="btn btn-outline-danger btn-sm float-right">
           <i className="fa fa-trash-o" />
--- todo-app/src/components/todo-list/todo-list.js
+++ todo-app/src/components/todo-list/todo-list.js
@@ -3,14 +3,15 @@ import './todo-list.css'
 
 import TodoListItem from '../todo-list-item';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onDeleted }) => {
 
   const elements = todos.map((item) => {
 
     const { id, ...itemProps } = item;
     return (
       <li key={ id } className="list-group-item">
-        <TodoListItem { ...itemProps } />
+        <TodoListItem { ...itemProps }
+        onDeleted={ () => onDeleted(id) }/>
       </li>
       /* </li>
         <TodoListItem
