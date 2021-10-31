ef099dbe30276e8e8297ccc8753c7ec08217d7c4
volkovVA <volkov.slava@hotmail.com>
2021-10-31 14:17:33 +0300

    feat: add search

--- todo-app/src/components/search-panel/search-panel.js
+++ todo-app/src/components/search-panel/search-panel.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './search-panel.css'
 
-const SearchPanel = () => {
+export default class SearchPanel extends Component {
 
-  return (
-    <input
-      type="text"
-      className="form-control search-input"
-      placeholder="type to search" />
-  )
+  state = {
+    term: ''
+  }
+
+  onSearchChange = (e) => {
+    const term = e.target.value;
+    this.setState({ term });
+    this.props.onSearchChange(term);
+  }
+
+  render() {
+    return (
+      <input
+        type="text"
+        className="form-control search-input"
+        placeholder="type to search"
+        value={ this.state.term }
+        onChange={ this.onSearchChange }
+      />
+    )
+  }
 }
-
-export default SearchPanel;
