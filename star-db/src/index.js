f96197424cffe13ff7922a046a6921112a24be06
volkovVA <volkov.slava@hotmail.com>
2021-11-03 19:19:45 +0300

    feat: add components layout

--- star-db/src/index.js
+++ star-db/src/index.js
@@ -1,50 +1,6 @@
-class SwapiService {
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-  _apiBase = 'https://swapi.dev/api';
-
-  async getResource(url) {
-    const res = await fetch(`${this._apiBase}${url}`);
-
-    if (!res.ok) {
-      throw new Error(`Could not fetch ${url}, received ${res.status}`)
-    }
-
-    return await res.json();
-  }
-
-  async getAllPeople() {
-    const res = await this.getResource(`/people/`);
-    return res.results;
-  }
-
-  getPerson(id) {
-    return this.getResource(`/people/${id}`);
-  }
-
-  async getAllPlanets() {
-    const res = await this.getResource(`/planets/`);
-    return res.results;
-  }
-
-  getPlanet(id) {
-    return this.getResource(`/planets/${id}`);
-  }
-
-  async getAllStarShips() {
-    const res = await this.getResource(`/starships/`);
-    return res.results;
-  }
-
-  getStarship(id) {
-    return this.getResource(`/starships/${id}`);
-  }
-}
-
-const swapi = new SwapiService();
-
-swapi.getPerson(3).then((people) => {
-  // people.forEach((person) => {
-  //   console.log(person.name);
-  // })
-  console.log(people.name);
-});
+ReactDOM.render(<App />,
+  document.getElementById('root'));
