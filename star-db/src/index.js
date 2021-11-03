50e6e162477a28b5517241d31cd6318dae3ee671
volkovVA <volkov.slava@hotmail.com>
2021-11-03 17:29:15 +0300

    feat: add fetch api

---
+++ star-db/src/index.js
@@ -0,0 +1,17 @@
+const getResource = async(url) => {
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url}, received ${res.status}`)
+  }
+  const body = await res.json();
+  return body
+}
+
+getResource('https://swapi.dev/api/people/123123/')
+  .then((body) => {
+    console.log(body);
+  })
+  .catch((err) => {
+    console.error('Hello', err);
+  });
