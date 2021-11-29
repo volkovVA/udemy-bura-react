1deca1282be8bfd6b7ca327ce98488b936faf33f
volkovVA <volkov.slava@hotmail.com>
2021-11-29 13:46:49 +0300

    feat: add use-state

---
+++ hooks/src/use-state.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <HookSwitcher />
+    </div>
+  )
+}
+
+const HookSwitcher = () => {
+
+  const [ color, setColor ] = useState('gray');
+  const [ fontSize, setFontSize ] = useState(14);
+
+  return (
+    <div style={{
+        padding: '10px',
+        background: color,
+        fontSize: `${fontSize}px` }}>
+      <h1>Hello World!!!</h1>
+      <button
+        onClick={ () => setColor('gray') }>
+          Dark
+      </button>
+      <button
+        onClick={ () => setColor('white') }>
+          Light
+      </button>
+      <button
+        onClick={ () => setFontSize((s) => s + 2) }
+      >+</button>
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
